2c18a7a8e6f48f814e0e8536a4a2e267a3317edd
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-28 10:38:54 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
-require("turrets");
-require("production");
-require("items");
+// items
+
+const stone = extend(Item, "stone", {});
+
+const denseAlloy = extend(Item, "dense-alloy", {});
+
+const crimsite = extend(Item, "crimsite", {});
+
+// blocks / production
+
+const denseSmelter = extend(GenericCrafter, "dense-smelter", {});
+
+const crimsiteSynthesizer = extend(GenericCrafter, "crimsite-synthesizer", {});
+
+// blocks / turrets
+
+const raven = extend(ItemTurret, "raven", {});
