01f610a4685d33fc6fffe9ff5333e4047546e10d
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-06-28 11:19:16 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,18 @@ const denseAlloy = extend(Item, "dense-alloy", {});
 
 const crimsite = extend(Item, "crimsite", {});
 
+const chargedIngot = extend(Item, "charged-ingot", {});
+
 // blocks / production
 
 const denseSmelter = extend(GenericCrafter, "dense-smelter", {});
 
 const crimsiteSynthesizer = extend(GenericCrafter, "crimsite-synthesizer", {});
 
+const chargeSmelter = extend(GenericCrafter, "charge-smelter", {});
+
 // blocks / turrets
 
 const raven = extend(PowerTurret, "raven", {});
+
+const blade = extend(ContinuousTurret, "blade", {});
