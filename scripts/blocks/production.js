1f9946d04a68f7aa7f2374ec5a5844da70964c7c
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 14:55:04 -0700

    Create production.js

---
+++ scripts/blocks/production.js
@@ -0,0 +1,9 @@
+const denseSmelter = extend(GenericCrafter, "dense-smelter", {});
+
+const crimsiteSynthesizer = extend(GenericCrafter, "crimsite-synthesizer", {});
+
+const chargeSmelter = extend(GenericCrafter, "charge-smelter", {});
+
+const astrogenChamber = extend(GenericCrafter, "astrogen-chamber", {});
+
+const astroliumCrystallizer = extend(GenericCrafter, "astrolium-crystallizer", {});
