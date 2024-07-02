6655d37bd0a9c5cb776e17d37e75d299313e5fb5
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 15:04:12 -0700

    Delete scripts/items.js

--- scripts/items.js
+++
@@ -1,9 +0,0 @@
-const stone = extend(Item, "stone", {});
-
-const denseAlloy = extend(Item, "dense-alloy", {});
-
-const crimsite = extend(Item, "crimsite", {});
-
-const chargedIngot = extend(Item, "charged-ingot", {});
-
-const astrolium = extend(Item, "astrolium", {});
