be48fa68b6d0d5c66d68fe6708f75f07d29e8bf6
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 14:55:41 -0700

    Create turrets.js

---
+++ scripts/blocks/turrets.js
@@ -0,0 +1,9 @@
+const raven = extend(PowerTurret, "raven", {});
+
+const slash = extend(PowerTurret, "slash", {});
+
+const blade = extend(ContinuousTurret, "blade", {});
+
+const twinkle = extend(PowerTurret, "twinkle", {});
+
+const corona = extend(ItemTurret, "corona", {});
