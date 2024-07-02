a6d9f0d9021bb016d9c75944d2f937058c961d64
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 12:48:22 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,5 @@ const slash = extend(PowerTurret, "slash", {});
 const blade = extend(ContinuousTurret, "blade", {});
 
 const twinkle = extend(PowerTurret, "twinkle", {});
+
+const corona = extend(ItemTurret, "corona", {});
