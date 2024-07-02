934fc3ea0ed4385ef78e0e2ca489da95dc3c1bee
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 10:31:32 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,12 @@ const crimsite = extend(Item, "crimsite", {});
 
 const chargedIngot = extend(Item, "charged-ingot", {});
 
+const astrolium = extend(Item, "astrolium", {});
+
+// liquids
+
+const astrogen = extend(Liquid, "astrogen", {});
+
 // blocks / production
 
 const denseSmelter = extend(GenericCrafter, "dense-smelter", {});
@@ -16,8 +22,16 @@ const crimsiteSynthesizer = extend(GenericCrafter, "crimsite-synthesizer", {});
 
 const chargeSmelter = extend(GenericCrafter, "charge-smelter", {});
 
+const astrogenChamber = extend(GenericCrafter, "astrogen-chamber", {});
+
+const astroliumCrystallizer = extend(GenericCrafter, "astrolium-crystallizer", {});
+
 // blocks / turrets
 
 const raven = extend(PowerTurret, "raven", {});
 
+const slash = extend(PowerTurret, "slash", {});
+
 const blade = extend(ContinuousTurret, "blade", {});
+
+const twinkle = extend(PowerTurret, "twinkle", {});
