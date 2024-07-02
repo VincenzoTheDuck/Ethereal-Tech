f523409cdc0739737a6ce946cf8d3d1a758ea263
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 15:04:48 -0700

    Delete scripts/blocks/turrets.js

--- scripts/blocks/turrets.js
+++
@@ -1,9 +0,0 @@
-const raven = extend(PowerTurret, "raven", {});
-
-const slash = extend(PowerTurret, "slash", {});
-
-const blade = extend(ContinuousTurret, "blade", {});
-
-const twinkle = extend(PowerTurret, "twinkle", {});
-
-const corona = extend(ItemTurret, "corona", {});
