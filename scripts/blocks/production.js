b668ccc924039debd0a87426a8171d07a5583345
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 15:04:38 -0700

    Delete scripts/blocks/production.js

--- scripts/blocks/production.js
+++
@@ -1,9 +0,0 @@
-const denseSmelter = extend(GenericCrafter, "dense-smelter", {});
-
-const crimsiteSynthesizer = extend(GenericCrafter, "crimsite-synthesizer", {});
-
-const chargeSmelter = extend(GenericCrafter, "charge-smelter", {});
-
-const astrogenChamber = extend(GenericCrafter, "astrogen-chamber", {});
-
-const astroliumCrystallizer = extend(GenericCrafter, "astrolium-crystallizer", {});
