6181c7e890d35dd452d58ad2d94530dffe9d582b
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-02 14:54:40 -0700

    Create items.js

---
+++ scripts/items.js
@@ -0,0 +1,9 @@
+const stone = extend(Item, "stone", {});
+
+const denseAlloy = extend(Item, "dense-alloy", {});
+
+const crimsite = extend(Item, "crimsite", {});
+
+const chargedIngot = extend(Item, "charged-ingot", {});
+
+const astrolium = extend(Item, "astrolium", {});
