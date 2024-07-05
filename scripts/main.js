087a6837e582ff9e19c510254037cd87f07c1a1b
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-05 10:21:45 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
+// items
+
+const ironOre = extend(Item, "iron-ore", {});
+
+const coke = extend(Item, "coke", {});
+
+const steel = extend(Item, "steel", {});
+
+// blocks/environment
+
+const oreIron = extend(OreBlock, "ore-iron", {});
+
 // blocks/power
 
 const batteryMedium = extend(Battery, "battery-medium", {});
+
+// blocks/production
+
+const cokeOven = extend(GenericCrafter, "coke-oven", {});
+
+const blastFurnace = extend(GenericCrafter, "blast-furnace", {});
