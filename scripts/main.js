a2faa7094c02138d744836059799d310b56010ff
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-07-09 14:49:47 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -43,6 +43,93 @@ const stormHit = new Effect(28, e => {
 	Angles.randLenVectors(e.id, 4, e.finpow() * 21.0, 0, 180.0, hl);
 });
 
+const dreadShootAlpha = new Effect(35, e => {
+	Draw.color(Color.valueOf("7a8cdc"));
+	Lines.stroke(e.fout() * 5)
+	Lines.square(e.x, e.y, e.fin() * 24, 45);
+	Lines.stroke(e.fout() * 2.5)
+	Lines.square(e.x, e.y, e.fin() * 12, 45);
+});
+
+const dreadShootBeta = new Effect(35, e => {
+	Draw.color(Color.valueOf("dc7de0"));
+	Lines.stroke(e.fout() * 5)
+	Lines.square(e.x, e.y, e.fin() * 24, 45);
+	Lines.stroke(e.fout() * 2.5)
+	Lines.square(e.x, e.y, e.fin() * 12, 45);
+});
+
+const dreadShootGamma = new Effect(35, e => {
+	Draw.color(Color.valueOf("96f58c"));
+	Lines.stroke(e.fout() * 5)
+	Lines.square(e.x, e.y, e.fin() * 24, 45);
+	Lines.stroke(e.fout() * 2.5)
+	Lines.square(e.x, e.y, e.fin() * 12, 45);
+});
+
+const dreadTrailAlpha = new Effect(25, e => {
+	Draw.color(Color.valueOf("7a8cdc"));
+	Lines.stroke(e.fout() * 4)
+	Lines.lineAngle(e.x, e.y, e.rotation, 10);
+});
+
+const dreadTrailBeta = new Effect(25, e => {
+	Draw.color(Color.valueOf("dc7de0"));
+	Lines.stroke(e.fout() * 4)
+	Lines.lineAngle(e.x, e.y, e.rotation, 10);
+});
+
+const dreadTrailGamma = new Effect(25, e => {
+	Draw.color(Color.valueOf("96f58c"));
+	Lines.stroke(e.fout() * 4)
+	Lines.lineAngle(e.x, e.y, e.rotation, 10);
+});
+
+const dreadHitAlpha = new Effect(25, e => {
+	Draw.color(Color.valueOf("7a8cdc"));
+        Lines.stroke(e.fout() * 4);
+        Lines.circle(e.x, e.y, 4 + e.finpow() * 25);
+
+        for(let i = 0; i < 4; i++){
+            Drawf.tri(e.x, e.y, 8, 100 * e.fout(), i*90 + 45);
+        }
+
+        Draw.color();
+        for(let i = 0; i < 4; i++){
+            Drawf.tri(e.x, e.y, 5, 55 * e.fout(), i*90 + 45);
+        }
+});
+
+const dreadHitBeta = new Effect(25, e => {
+	Draw.color(Color.valueOf("dc7de0"));
+        Lines.stroke(e.fout() * 4);
+        Lines.circle(e.x, e.y, 4 + e.finpow() * 25);
+
+        for(let i = 0; i < 4; i++){
+            Drawf.tri(e.x, e.y, 8, 100 * e.fout(), i*90 + 45);
+        }
+
+        Draw.color();
+        for(let i = 0; i < 4; i++){
+            Drawf.tri(e.x, e.y, 5, 55 * e.fout(), i*90 + 45);
+        }
+});
+
+const dreadHitGamma = new Effect(25, e => {
+	Draw.color(Color.valueOf("96f58c"));
+        Lines.stroke(e.fout() * 4);
+        Lines.circle(e.x, e.y, 4 + e.finpow() * 25);
+
+        for(let i = 0; i < 4; i++){
+            Drawf.tri(e.x, e.y, 8, 100 * e.fout(), i*90 + 45);
+        }
+
+        Draw.color();
+        for(let i = 0; i < 4; i++){
+            Drawf.tri(e.x, e.y, 5, 55 * e.fout(), i*90 + 45);
+        }
+});
+
 // bullets
 
 const stormBullet = extend(BasicBulletType, {
@@ -60,9 +147,82 @@ const stormBullet = extend(BasicBulletType, {
 	}
 });
 
-require("items");
+const dreadBoltAlpha = extend(PointBulletType, {
+  	damage: 2750,
+	speed: range,
+	shootEffect: dreadShootAlpha,
+	hitShake: 6.5,
+	trailSpacing: 10,
+	buildingDamageMultiplier: 0.12,
+	trailEffect: dreadTrailAlpha,
+	hitEffect: dreadHitAlpha,
+	despawnEffect: Fx.none,
+	despawnHit: true,
+	ammoMultiplier: 1
+});
 
-// environment
+const dreadBoltBeta = extend(PointBulletType, {
+  	damage: 3250,
+	lightning: 6,
+	lightningLength: 9,
+	lightningLengthRand: 3,
+	lightningDamage: 770,
+	lightningColor: Color.valueOf("dc7de0"),
+	speed: range,
+	shootEffect: dreadShootBeta,
+	hitShake: 9,
+	trailSpacing: 10,
+	buildingDamageMultiplier: 0.12,
+	trailEffect: dreadTrailBeta,
+	hitEffect: dreadHitBeta,
+	despawnEffect: Fx.none,
+	despawnHit: true,
+	ammoMultiplier: 1,
+	reloadMultiplier: 0.88
+});
+
+const dreadBoltGamma = extend(PointBulletType, {
+  	damage: 4920,
+	lightning: 9,
+	lightningLength: 11,
+	lightningLengthRand: 4,
+	lightningDamage: 920,
+	lightningColor: Color.valueOf("96f58c"),
+	speed: range,
+	lifetime: 30,
+	shootEffect: dreadShootGamma,
+	hitShake: 15,
+	trailSpacing: 10,
+	buildingDamageMultiplier: 0.12,
+	trailEffect: dreadTrailGamma,
+	hitEffect: dreadHitGamma,
+	despawnEffect: Fx.none,
+	despawnHit: true,
+	ammoMultiplier: 1,
+	reloadMultiplier: 0.63
+});
+
+// items
+
+const ironOre = extend(Item, "iron-ore", {});
+
+const coke = extend(Item, "coke", {});
+
+const steel = extend(Item, "steel", {});
+
+const quarzite = extend(Item, "quarzite", {});
+
+const ruxonium = extend(Item, "ruxonium", {});
+
+const osmium = extend(Item, "osmium", {});
+
+const alphaSteel = extend(Item, "alpha-steel", {});
+
+const betaSteel = extend(Item, "beta-steel", {});
+
+const gammaSteel = extend(Item, "gamma-steel", {});
+
+// blocks/environment
 
 const oreIron = extend(OreBlock, "ore-iron", {});
 
@@ -70,11 +230,11 @@ const oreQuarzite = extend(OreBlock, "ore-quarzite", {});
 
 const oreOsmium = extend(OreBlock, "ore-osmium", {});
 
-// power
+// blocks/power
 
 const batteryMedium = extend(Battery, "battery-medium", {});
 
-// production
+// blocks/production
 
 const cokeOven = extend(GenericCrafter, "coke-oven", {});
 
@@ -88,7 +248,7 @@ const betaFurnace = extend(GenericCrafter, "beta-furnace", {});
 
 const gammaForge = extend(GenericCrafter, "gamma-forge", {});
 
-// turrets
+// blocks/turrets
 
 const shock = extend(PowerTurret, "shock", {});
 
@@ -117,9 +277,15 @@ const shatter = extend(ItemTurret, "shatter", {});
 
 const tempest = extend(ItemTurret, "tempest", {});
 
-require("turrets/dread");
+const dread = extend(ItemTurret, "dread", {});
+dread.range = range
+dread.ammo(
+  alphaSteel, dreadBoltAlpha,
+  betaSteel, dreadBoltBeta,
+  gammaSteel, dreadBoltGamma
+);
 
-// walls
+// blocks/walls
 
 const wallHealthMultiplier = 4
 
@@ -179,13 +345,13 @@ const gammaWallLarge = extend(Wall, "gamma-wall-large", {
   health: 520 * 4 * wallHealthMultiplier
 });
 
-// drills
+// blocks/drills
 
 const arcDrill = extend(Drill, "arc-drill", {
 	updateEffect: pulverizeBlue
 });
 
-// distribution
+// blocks/distribution
 
 const ruxoniumConveyor = extend(StackConveyor, "ruxonium-conveyor", {
 	loadEffect: ruxoniumConveyorLoad,
